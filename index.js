fc94903c9438ba62bce4ea96be763b4468cadaab
bhargavreddy982 <102218460+bhargavreddy982@users.noreply.github.com>
2024-02-18 22:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Post=require('./models/Post');
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
-app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+app.use(cors({credentials:true,origin:"https://front-end-services-blogs.onrender.com"}));
 app.use(express.json());
 //cookie parser"
 app.use(cookieParser());
